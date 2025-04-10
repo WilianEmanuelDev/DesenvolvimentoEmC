7e3aa130109ca15a0582b290bc778a0bfae1f9b5
Wilian Emanuel de Carvalho <wilianemanuel3012@gmail.com>
2025-04-09 23:33:35 -0300

    Update ScanfePrintf.c

--- ScanfePrintf.c
+++ ScanfePrintf.c
@@ -16,19 +16,5 @@ int main() {
     printf("A Idade do %s é: %d\n", nome, idade);
     printf("A Altura do %s é: %.2f\n", nome, altura);
 
- /*
- %d: Imprime um inteiro no formato decimal.
- 
-%i: Equivalente a %d.
- 
-%f: Imprime um número de ponto flutuante no formato padrão.
- 
-%e: Imprime um número de ponto flutuante na notação científica.
- 
-%c: Imprime um único caractere.
- 
-%s: Imprime uma cadeia (string) de caracteres.
-
-*/
     return 0;
 }
